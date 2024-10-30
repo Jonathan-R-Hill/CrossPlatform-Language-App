ce475dea4561dd8f6bbe4adc21e78539974a280b
Jonathan Hill <jonathan.r.hill@outlook.com>
2024-10-30 16:40:14 +0000

    updated phone package & rebuilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.app"
+        applicationId = "com.languageTest.app"
         minSdk = 28
         targetSdk = 34
         versionCode = 1
